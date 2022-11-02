f6a875317bc4e91562fd54d87fc19ea3a401676e
AndrewSavelyev <andreys.work911@gmail.com>
2022-11-02 07:42:33 +0200

    task 3

---
+++ Lesson_2/task3.py
@@ -0,0 +1,5 @@
+#Print character "O"
+print ("#########", "#\t\t#", "#\t\t#", "#\t\t#", "#########",end='\n\n',sep='\n')
+
+#Print character "H"
+print ("#\t\t#", "#\t\t#", "#########", "#\t\t#", "#\t\t#",sep='\n')
