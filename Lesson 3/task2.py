0c3f80b08e6dc135fd69cd79f72d2d0527f26627
AndrewSavelyev <andreys.work911@gmail.com>
2022-11-06 09:47:20 +0200

    lesson 3

---
+++ Lesson 3/task2.py
@@ -0,0 +1,7 @@
+# Manipulate strings.
+#
+# Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+
+first_name = "Andrew"
+last_name = "Savelyev"
+print("Greeting," + " " + first_name + " " + last_name, end='!')
