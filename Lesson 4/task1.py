0f0aa702e81e99dbe389b4e91acdc146bdf9d21b
Andrew Savelyev <andreys.work911@gmail.com>
2022-11-15 21:04:12 +0200

    class for Lesson11

---
+++ Lesson 4/task1.py
@@ -0,0 +1,46 @@
+# String manipulation
+#
+# Write a Python program to get a string made of the first 2 and the last 2 chars from a given string. If the string length is less than 2, return instead of the empty string.
+#
+# Sample String: 'helloworld'
+#
+# Expected Result : 'held'
+#
+# Sample String: 'my'
+#
+# Expected Result : 'mymy'
+#
+# Sample String: 'x'
+#
+# Expected Result: Empty String
+#
+# Tips:
+#
+#     Use built-in function len() on an input string
+#     Use positive indexing to get the first characters of a string and negative indexing to get the last characters
+string = 'helloworld'
+i = 0
+while i < len(string):
+    if i == 0:
+        print(string[i])
+        i += 1
+        continue
+    elif i == 1:
+        print(string[i])
+        i += 1
+        continue
+    i += 1
+i = len(string)
+while len(string) > 1:
+    if i == 2:
+        print(string[-i])
+        i -= 1
+        continue
+    elif i == 1:
+        print(string[-i])
+        i -= 1
+        break
+    i -= 1
+
+
+
