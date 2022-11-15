8fdc353348a420eb4b791d12743d3cd2ca71f50f
Andrew Savelyev <andreys.work911@gmail.com>
2022-11-15 22:42:11 +0200

    Lesson 4 task 2

--- Lesson 4/task2.py
+++ Lesson 4/task2.py
@@ -5,10 +5,11 @@
 # Make a program that checks if a string is in the right format for a phone number. The program should check that the string contains only numerical characters and is only 10 characters long. Print a suitable message depending on the outcome of the string evaluation.
 print('Enter phone number:')
 x = input()
+ph = 0
 check = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 for i in range(len(x)):
     if x[i] in check:
-        print("Phone is correct")
+        ph += 1
     else:
         print("Phone is incorrect")
 
