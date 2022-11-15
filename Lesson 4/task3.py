80a21db96f7a26a42ff941e3a6f141889a172e68
Andrew Savelyev <andreys.work911@gmail.com>
2022-11-16 00:48:20 +0200

    Lesson 4 task 3

---
+++ Lesson 4/task3.py
@@ -0,0 +1,11 @@
+# Task 3
+#
+# The math quiz program.
+#
+# Write a program that asks the answer for a mathematical expression, checks whether the user is right or wrong, and then responds with a message accordingly.
+print("How many 6 + 3 is?")
+x = int(input())
+if x == 9:
+    print("Correct")
+else:
+    print("Incorrect")
