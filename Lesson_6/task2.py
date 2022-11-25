f1b770da51711ae2d6156609ee49190b9ac12aed
Andrew Savelyev <andreys.work911@gmail.com>
2022-11-26 01:52:33 +0200

    Questions Lesson_4

--- Lesson_6/task2.py
+++ Lesson_6/task2.py
@@ -16,28 +16,37 @@ print(list_1)
 print(list_2)
 j = 0
 count = 0
-list_4 = []
+#list_4 = []
 #define list_4, which is "third list"
+# while j < 9:
+#     if list_1[j] == list_2[j]:
+#         list_4.insert(j, list_1[j])
+#         j += 1
+#     j += 1
+#
+# #define if exists duplicates in list
+# k = 0
+# while k < len(list_4):
+#     if list_4.count(list_4[k]) > 1:
+#         list_4.remove(list_4[k])
+#         k += 1
+#         count += 1
+#     k += 1
+# k += 1
+#
+# if list_4 == []:
+#     print("There is no commom integers")
+# else:
+#     print(list_4)
+list_3 = []
+list_4 = {}
 while j < 9:
-    if list_1[j] == list_2[j]:
-        list_4.insert(j, list_1[j])
-        j += 1
-    j += 1
+     if list_1[j] == list_2[j]:
+         list_3.insert(j, list_1[j])
+         list_4 = set(list_3)
+     j += 1
 
-#define if exists duplicates in list
-k = 0
-while k < len(list_4):
-    if list_4.count(list_4[k]) > 1:
-        list_4.remove(list_4[k])
-        k += 1
-        count += 1
-    k += 1
-k += 1
-
-if list_4 == []:
-    print("There is no commom integers")
+if list_4 == {}:
+     print("There is no commom integers")
 else:
-    print(list_4)
-
-
-
+     print(list_4)
