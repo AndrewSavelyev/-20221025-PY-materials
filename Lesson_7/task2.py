e4f58017556aa2bd28907b4757f0ef19bbcf897e
Andrew Savelyev <andreys.work911@gmail.com>
2022-11-27 02:51:22 +0200

    Lesson_7 task2

--- Lesson_7/task2.py
+++ Lesson_7/task2.py
@@ -16,3 +16,21 @@
 #
 # Compute the total price of the stock where the total price is the sum of the price of an item multiplied by the quantity of this exact item.
 
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+total_price = 0
+for key, value in prices.items():
+    price = value * stock[key]
+    print("price of", key, "=", price)
+    total_price = total_price + price
+print("Total price = ", total_price)
