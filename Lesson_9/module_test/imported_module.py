4d4590946ed5b0bfc89e95fb68e4b1549c402349
Andrew Savelyev <andreys.work911@gmail.com>
2022-11-29 04:22:34 +0200

    Lesson8 task3 update

---
+++ Lesson_9/module_test/imported_module.py
@@ -0,0 +1,3 @@
+print("From directory module_test we imported module imported_module.py")
+
+imported_var = 1000
---
+++ Lesson_9/task_2.py
@@ -0,0 +1,16 @@
+# The sys module.
+#
+#  The “sys.path” list is initialized from the PYTHONPATH environment variable. Is it possible to change it from within
+# Python? If so, does it affect where Python looks for module files? Run some interactive tests to find it out.
+
+#Answer: Yes, we can. Firstiful Python looks in initial file __init__.py
+#Then we can proclaim where Python must look for modules via directive
+#In this sample i've created module imported_module.py in subdirectory module_test
+#and from there imported variable imported_var
+
+#import sys
+from module_test.imported_module import imported_var
+
+#print(sys.path)
+print(imported_var)
+
