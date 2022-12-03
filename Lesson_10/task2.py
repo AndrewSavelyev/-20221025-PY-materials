dbf44c06aa2a60313cbb38838aa02d3b58bad886
Andrew Savelyev <andreys.work911@gmail.com>
2022-12-03 05:01:38 +0200

    Lesson_10 task2

--- Lesson_10/task2.py
+++ Lesson_10/task2.py
@@ -1,3 +1,21 @@
 # Write a function that takes in two numbers from the user via input(), call the numbers a and b, and then returns the
 # value of squared a divided by b, construct a try-except block which raises an exception if the two values given by the
 # input function were not numbers, and if value b was zero (cannot divide by zero).
+
+a = input('Enter first number:')
+b = input('Enter second number:')
+
+def two_numbers():
+    try:
+        print(int(a)**2/int(b))
+    except TypeError:
+        print('Values must be integers')
+    except ValueError:
+        print('Values must be integers')
+    except ZeroDivisionError:
+        print('Second value must not be zero')
+
+two_numbers()
+
+
+
